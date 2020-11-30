382b4c879d74bea133bd904255e6a6bdac72f9ac
Katarzyna Duchowska <k.duchowska@o2.pl>
2020-11-30 18:07:06 +0100

    Z1 - walidacja numeru

--- Z1/script.js
+++ Z1/script.js
@@ -29,7 +29,73 @@ function addElement() {
 
 function validate(object) {
 
-    //todo
+    // walidujemy wszystkie pola
+    const validNumber = validateNumber(object.number);
+    const validRadio = validateRadio(object.favouriteNumber);
+    const validPassword = validatePassword(object.password);
+    const validRepeatedPassword = validateRepeatedPassword(object.password, object.password2);
+
+    // zwracamy wynik walidacji - że formularz ją przeszedł, wszystkie pola muszą być wypełnione poprawnie
+    return validNumber && validRadio && validPassword && validRepeatedPassword;
+}
+
+function validateNumber(number) {
+
+    // walidujemy numer, podany w argumencie - w tym wypadku sprawdzamy, czy pasuje do wzorca
+    const valid = /^\d+([.,]\d{1,2})?$/.test(number);
+
+    // odszukujemy na stronie odpowiednie pole - input, w którym został wpisany numer
+    const input = document.querySelector("input[name='number']");
+
+    if (valid) {
+        // jeśli numer pasuje do wzorca - usuwamy ewentualne komunikaty walidacyjne, jeśli są
+
+        // ustawiamy pustą klasę - w razie jakby input był wcześniej oznaczony na czerwono
+        input.className = "";
+
+        const nameMessage = document.getElementById("number-input-message");
+        if (nameMessage) {
+            //jeśli wyświetla się komunikat - usuwamy go
+            nameMessage.parentElement.removeChild(nameMessage);
+        }
+    } else {
+        // numer nie pasuje do wzorca - dodajemy komunikaty walidacyjne, jeśli ich nie ma
+
+        // dodajemy inputowi klasę, która oznacza, że coś z nim nie tak
+        input.className = "invalid";
+
+        // sprawdzamy, czy wyświetla się komunikat o błędzie w polu z numerem
+        if (!document.getElementById("number-input-message")) {
+            // tworzymy element, który będzie mówił o błędzie w wybranym polu
+            const small = document.createElement("small");
+            small.id = "number-input-message"; // nadajemy id - potem dzięki niemu dostaniemy się do elementu, żeby go usunąć
+            small.className = "invalid"; // nadajemy klasę - żeby był czerwony
+            small.innerText = "Niepoprawny numer - dopuszczalna tylko dodatnia liczba z maksymalnie dwoma cyframi po przecinku"; // dodajemy tekst, który wyświetli się użytkownikowi
+
+            // doczepiamy element jako "rodzeństwo" inputa
+            input.parentElement.appendChild(small);
+        }
+    }
+
+    // zwracamy wynik walidacji
+    return valid;
+}
+
+function validateRadio(radio) {
+    // todo
+
+    return true;
+}
+
+function validatePassword(password) {
+    // regex do hasła: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+    // todo
+
+    return true;
+}
+
+function validateRepeatedPassword(password, repeatedPassword) {
+    // todo
 
     return true;
 }
